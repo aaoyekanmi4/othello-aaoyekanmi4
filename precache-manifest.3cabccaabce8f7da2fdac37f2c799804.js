161f9582be8e2ca484fe603b56a562d02d9f5e0c
Arit Oyekan <aaoyekanmi4@gmail.com>
2022-10-08 13:43:23 -0500

    Updates

---
+++ precache-manifest.3cabccaabce8f7da2fdac37f2c799804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91625abb8b1b2f14bb4379a1988b087c",
+    "url": "/othello-aaoyekanmi4/index.html"
+  },
+  {
+    "revision": "9a919e9875c374006a93",
+    "url": "/othello-aaoyekanmi4/static/css/main.62e5e904.chunk.css"
+  },
+  {
+    "revision": "1fa3ca19a0ca495b76d4",
+    "url": "/othello-aaoyekanmi4/static/js/2.443c7d2e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/othello-aaoyekanmi4/static/js/2.443c7d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a919e9875c374006a93",
+    "url": "/othello-aaoyekanmi4/static/js/main.c600ebbd.chunk.js"
+  },
+  {
+    "revision": "2fe966d404acb177cfb2",
+    "url": "/othello-aaoyekanmi4/static/js/runtime-main.c15ecc62.js"
+  }
+]);
--- src/Piece/Piece.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Piece.css";
-const Piece = (props) => {
-  return <div class="piece" style={{ backgroundColor: props.color }}></div>;
-};
-
-export default Piece;
